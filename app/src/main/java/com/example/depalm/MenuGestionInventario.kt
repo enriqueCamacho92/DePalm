65076928fa8228777f624de6924609d0b340cc98
Oziel Enrique Camacho Rodriguez <enrique_camacho92@hotmail.com>
2022-03-18 19:09:40 -0600

    Interfaces
    Inicio de proyecto. Se irán creando las interfaces

---
+++ app/src/main/java/com/example/depalm/MenuGestionInventario.kt
@@ -0,0 +1,11 @@
+package com.example.depalm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuGestionInventario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_gestion_inventario)
+    }
+}
